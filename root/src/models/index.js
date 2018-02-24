093b55e94b4c8c01b5f14540c0cca308f151433d
yinfxs <yinfxs@gmail.com>
2018-02-24 23:32:40 +0800

    added Toast

--- root/src/models/index.js
+++ root/src/models/index.js
@@ -1,6 +1,7 @@
 import qs from 'qs';
 import * as apiService from '../services/api';
 import jweixin from '../utils/jweixin';
+import { Toast } from 'antd-mobile';
 
 export default {
   namespace: 'index',
@@ -19,7 +20,11 @@ export default {
     * jssdk_config({ payload }, { put, call, select }) {
       const { data, errcode, errmsg } = yield call(apiService.get, { url: `/jssdk/config?${qs.stringify(payload)}` });
       if (!errcode && !errmsg && data) {
-        jweixin(Object.assign({ debug: true }, data));
+        jweixin(data, () => {
+          Toast.info(`初始化成功`);
+        }, (res) => {
+          Toast.fail(`初始化失败：${JSON.stringify(res, null, 0)}`);
+        });
         yield put({ type: 'updateJSSDKConfig', payload: data });
       }
     },
