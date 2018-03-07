8279c2f5ca809dec8e2196a8218f14685e6e81ee
yinfxs <yinfxs@gmail.com>
2018-03-07 23:04:50 +0800

    开启debug

--- root/src/utils/jweixin.js
+++ root/src/utils/jweixin.js
@@ -1,7 +1,7 @@
 import wx from 'weixin-js-sdk';
 
 export default function (obj, onReady, onError) {
-  wx.config(Object.assign({ debug: false }, obj));
+  wx.config(Object.assign({ debug: true }, obj));
   wx.ready(onReady);
   wx.error(onError);
 }
